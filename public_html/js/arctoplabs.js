0855e5826fc369778dff5d1821e19a3a3abc35da
dushyantfst <dushyant_fst@yahoo.com>
2017-11-22 13:33:34 +0530

    revisited

--- public_html/js/arctoplabs.js
+++ public_html/js/arctoplabs.js
@@ -7,7 +7,7 @@
 
 var appName = "demo";
 var url = 'https://api.masterdatanode.com/' + appName + '/vote/';
-var access_token = 'Zs0nTQB-ujOSV0KmEoPhBx2E6-Ab_GKO';  //'######access_token###########'
+var access_token = '######access_token###########';  //'######access_token###########'
 var content_type = 'application/json';
 var html5sticky = {};
 html5sticky.voteResult = function () {
